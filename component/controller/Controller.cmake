9c64b5faf333e30c39bba7628bedd9aa2456fb88
htalbot <hugo.talbot@inria.fr>
2017-08-31 15:31:33 +0200

    FIX compilation (Suture) and scene Tutorial

--- component/controller/Controller.cmake
+++ component/controller/Controller.cmake
@@ -3,12 +3,12 @@ list(APPEND HEADER_FILES
   component/controller/AdaptiveBeamController.inl
   component/controller/InterventionalRadiologyController.h
   component/controller/InterventionalRadiologyController.inl
-  component/controller/SutureController.h
-  component/controller/SutureController.inl
+  # component/controller/SutureController.h
+  # component/controller/SutureController.inl
   )
 
 list(APPEND SOURCE_FILES
   component/controller/AdaptiveBeamController.cpp
   component/controller/InterventionalRadiologyController.cpp
-  component/controller/SutureController.cpp
+  # component/controller/SutureController.cpp
   )
