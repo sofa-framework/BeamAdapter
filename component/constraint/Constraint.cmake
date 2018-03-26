e9b94c9fcebae8208e5877e7e0aec2969fe14610
Margaret Koehler <misk92@gmail.com>
2018-03-26 14:54:10 -0700

    [temp] remove ImplicitSurfaceAdaptiveConstraint because it's causing compilation errors'

--- component/constraint/Constraint.cmake
+++ component/constraint/Constraint.cmake
@@ -3,13 +3,13 @@ list(APPEND HEADER_FILES
   component/constraint/AdaptiveBeamSlidingConstraint.inl
   component/constraint/AdaptiveBeamLengthConstraint.h
   component/constraint/AdaptiveBeamLengthConstraint.inl
-  component/constraint/ImplicitSurfaceAdaptiveConstraint.h
-  component/constraint/ImplicitSurfaceAdaptiveConstraint.inl
+  #component/constraint/ImplicitSurfaceAdaptiveConstraint.h
+  #component/constraint/ImplicitSurfaceAdaptiveConstraint.inl
   )
 
 list(APPEND SOURCE_FILES
   component/constraint/AdaptiveBeamSlidingConstraint.cpp
   component/constraint/AdaptiveBeamLengthConstraint.cpp
-  component/constraint/ImplicitSurfaceAdaptiveConstraint.cpp
+  #component/constraint/ImplicitSurfaceAdaptiveConstraint.cpp
   )
 
