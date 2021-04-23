1503bae282c43e991bf35c66e4897363f719c89d
Camille <camille.krewcun@uca.fr>
2021-04-23 17:13:13 +0200

    Fix inclusions and typedefs (minor)

--- component/forcefield/AdaptivePlasticBeamForceField.h
+++ component/forcefield/AdaptivePlasticBeamForceField.h
@@ -36,6 +36,9 @@
 //////////////////////// Inclusion of headers...from wider to narrower/closer //////////////////////
 #include <sofa/core/visual/VisualParams.h>
 
+#include <sofa/helper/fixed_array.h>
+
+#include "../BeamPlasticInterpolation.h"
 #include "AdaptiveBeamForceFieldAndMass.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -44,11 +47,6 @@
 namespace sofa::plugin::beamadapter::component::forcefield
 {
 
-/////////////////////////////////// private namespace pattern //////////////////////////////////////
-/// To avoid the lacking of names imported with with 'using' in the other's component namespace
-/// you should use a private namespace and "export" only this one in the public namespace.
-/// This is done at the end of this file, have a look if you are not used to this pattern.
-////////////////////////////////////////////////////////////////////////////////////////////////////
 namespace _adaptiveplasticbeamforcefield_
 {
 
@@ -59,8 +57,10 @@ using sofa::defaulttype::Vec;
 using sofa::defaulttype::Mat;
 using sofa::core::behavior::MultiMatrixAccessor;
 using sofa::defaulttype::Rigid3Types;
+using sofa::defaulttype::SolidTypes;
 
-using sofa::component::fem::BeamInterpolation;
+using sofa::plugin::beamadapter::component::forcefield::_beamplasticinterpolation_::BeamPlasticInterpolation;
+using sofa::plugin::beamadapter::component::forcefield::_beamplasticinterpolation_::BeamGeometry;
 using sofa::component::forcefield::_adaptivebeamforcefieldandmass_::AdaptiveBeamForceFieldAndMass;
 
 /*!
@@ -79,13 +79,19 @@ public:
     SOFA_CLASS(SOFA_TEMPLATE(AdaptivePlasticBeamForceField, DataTypes),
         SOFA_TEMPLATE(AdaptiveBeamForceFieldAndMass, DataTypes));
 
+    typedef typename DataTypes::VecCoord VecCoord;
     typedef typename DataTypes::VecDeriv VecDeriv;
-
+    typedef typename Coord::value_type Real;
     typedef Data<VecCoord> DataVecCoord;
     typedef Data<VecDeriv> DataVecDeriv;
     typedef AdaptiveBeamForceFieldAndMass<DataTypes>::BeamLocalMatrices BeamLocalMatrices;
+    typedef BeamPlasticInterpolation<DataTypes> BPInterpolation;
+
+    typedef typename SolidTypes<Real>::Transform Transform;
+    typedef typename SolidTypes<Real>::SpatialVector SpatialVector;
 
     typedef Vec<3, Real> Vec3;
+    typedef Vec<6, Real> Vec6;
     typedef Vec<9, Real> Vec9;          ///< Second-order tensor in vector notation
     typedef Mat<9, 9, Real> Matrix9x9;  ///< Fourth-order tensor in vector notation
 
@@ -107,8 +113,8 @@ public:
      */
 protected:
 
-    class BeamPlasticVariables {
-
+    class BeamPlasticVariables
+    {
     public:
         BeamPlasticVariables() {}
         BeamPlasticVariables(const BeamPlasticVariables& v)
@@ -139,18 +145,19 @@ public:
     /////////////////////////////////////
     /// ForceField Interface
     /////////////////////////////////////
-    virtual void addForce(const MechanicalParams* mparams,
-        DataVecDeriv& f, const DataVecCoord& x, const DataVecDeriv& v);
+    virtual void addForce(const MechanicalParams* mparams, DataVecDeriv& dataf,
+                          const DataVecCoord& datax, const DataVecDeriv& v);
 
-    virtual void addDForce(const MechanicalParams* mparams,
-        DataVecDeriv& datadF, const DataVecDeriv& datadX);
+    virtual void addDForce(const MechanicalParams* mparams, DataVecDeriv& datadF,
+                           const DataVecDeriv& datadX);
 
-    void addKToMatrix(const MechanicalParams* mparams,
-        const MultiMatrixAccessor* matrix);
+    void addKToMatrix(const MechanicalParams* mparams, const MultiMatrixAccessor* matrix);
 
     void computeStiffness(int beam, BeamLocalMatrices& beamLocalMatrices);
 
 protected:
+
+    SingleLink<AdaptiveBeamForceFieldAndMass<DataTypes>, BPInterpolation, BaseLink::FLAG_STOREPATH | BaseLink::FLAG_STRONGLINK> l_interpolation;
 
     vector<BeamPlasticVariables> m_plasticVariables;
 
