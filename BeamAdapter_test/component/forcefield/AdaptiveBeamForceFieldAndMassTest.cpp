b19d44374e202561ce2e732420736fd53eb42ef6
epernod <erik.pernod@gmail.com>
2022-08-08 23:09:07 +0200

    [Tests] reactivate test on componentState check

--- BeamAdapter_test/component/forcefield/AdaptiveBeamForceFieldAndMassTest.cpp
+++ BeamAdapter_test/component/forcefield/AdaptiveBeamForceFieldAndMassTest.cpp
@@ -126,8 +126,7 @@ struct AdaptiveBeamForceFieldAndMassTest : public sofa::testing::BaseSimulationT
 
         // Check component state and Data default values
         
-        // TODO: activate this test in component init refactoring PR
-        //ASSERT_EQ(beamForceFieldMass->d_componentState.getValue(), sofa::core::objectmodel::ComponentState::Valid);
+        ASSERT_EQ(beamForceFieldMass->d_componentState.getValue(), sofa::core::objectmodel::ComponentState::Valid);
         ASSERT_EQ(beamForceFieldMass->d_computeMass.getValue(), true);
         ASSERT_FLOAT_EQ(beamForceFieldMass->d_massDensity.getValue(), 10.0);
         ASSERT_FLOAT_EQ(beamForceFieldMass->rayleighMass.getValue(), 0.0);
