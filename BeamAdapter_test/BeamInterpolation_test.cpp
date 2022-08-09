8cf6d8d4a8e6a5b7d36aa5d73ea6b95b6d53f66f
Frederick Roy <froy@lnrobo.com>
2022-08-09 09:48:57 +0900

    fix test compilation

--- BeamAdapter_test/BeamInterpolation_test.cpp
+++ BeamAdapter_test/BeamInterpolation_test.cpp
@@ -22,7 +22,7 @@
 #include <sofa/testing/BaseSimulationTest.h>
 
 #include <sofa/helper/BackTrace.h>
-
+#include <sofa/core/ExecParams.h>
 #include <sofa/component/statecontainer/MechanicalObject.h>
 
 using sofa::core::topology::BaseMeshTopology ;
@@ -88,8 +88,8 @@ struct BeamInterpolationTest : public  sofa::testing::BaseSimulationTest,
             Node::SPtr root = SceneLoaderXML::loadFromMemory ( "test1", scene.c_str(), scene.size());
             ASSERT_NE(root.get(), nullptr);
 
-            root->init(ExecParams::defaultInstance());
-            root->reinit(ExecParams::defaultInstance()) ;
+            root->init(core::ExecParams::defaultInstance());
+            root->reinit(core::ExecParams::defaultInstance()) ;
         }else if(lines[2]=="W")
         {
             EXPECT_MSG_EMIT(Error) ;
@@ -98,8 +98,8 @@ struct BeamInterpolationTest : public  sofa::testing::BaseSimulationTest,
             Node::SPtr root = SceneLoaderXML::loadFromMemory ( "test1", scene.c_str(), scene.size());
             ASSERT_NE(root.get(), nullptr);
 
-            root->init(ExecParams::defaultInstance());
-            root->reinit(ExecParams::defaultInstance()) ;
+            root->init(core::ExecParams::defaultInstance());
+            root->reinit(core::ExecParams::defaultInstance()) ;
         }
     }
 };
