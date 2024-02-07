7444f8c9b5ccf8cad2b634ab75e4fa0c50c02512
Alex Bilger <alxbilger@users.noreply.github.com>
2024-02-07 11:06:06 +0100

    Move cuda instantiations in a plugin (#132)
    * Introduce CUDA extension
    
    * Move cuda instantiations in an plugin
    
    * Change folder hierarchy according to the guidelines

---
+++ extensions/CUDA/src/BeamAdapter/CUDA/init.cpp
@@ -0,0 +1,67 @@
+/******************************************************************************
+*                              BeamAdapter plugin                             *
+*                  (c) 2006 Inria, University of Lille, CNRS                  *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: see Authors.md                                                     *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#include <BeamAdapter/CUDA/init.h>
+#include <BeamAdapter/initBeamAdapter.h>
+#include <sofa/core/ObjectFactory.h>
+namespace beamadapter::cuda
+{
+
+extern "C" {
+    SOFA_EXPORT_DYNAMIC_LIBRARY void initExternalModule();
+    SOFA_EXPORT_DYNAMIC_LIBRARY const char* getModuleName();
+    SOFA_EXPORT_DYNAMIC_LIBRARY const char* getModuleVersion();
+    SOFA_EXPORT_DYNAMIC_LIBRARY const char* getModuleComponentList();
+}
+
+void initExternalModule()
+{
+    init();
+}
+
+const char* getModuleName()
+{
+    return MODULE_NAME;
+}
+
+const char* getModuleVersion()
+{
+    return MODULE_VERSION;
+}
+
+void init()
+{
+    static bool first = true;
+    if (first)
+    {
+        sofa::component::initBeamAdapter();
+        first = false;
+    }
+}
+
+const char* getModuleComponentList()
+{
+    /// string containing the names of the classes provided by the plugin
+    static std::string classes = sofa::core::ObjectFactory::getInstance()->listClassesFromTarget(MODULE_NAME);
+    return classes.c_str();
+}
+
+} // namespace beamadapter::cuda
