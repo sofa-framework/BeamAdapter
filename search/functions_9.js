26474ecd443961ab84e8b559b7768f0245de5e4e
hugtalbot <hugo.talbot@sofa-framework.org>
2025-11-06 09:25:22 +0000

    Deploying to gh-pages from  @ a4e5dafc471d11fb511867736233f3af447b8f88 🚀

--- search/functions_9.js
+++ search/functions_9.js
@@ -4,5 +4,5 @@ var searchData=
   ['rodspiresection_1',['RodSpireSection',['../classbeamadapter_1_1_rod_spire_section.html#abbdb28e12e8b7a9fa7419e7d94cb1de3',1,'beamadapter::RodSpireSection']]],
   ['rodstraightsection_2',['RodStraightSection',['../classbeamadapter_1_1_rod_straight_section.html#ae98b6ce6e2d90fde28e90e54d9e451f9',1,'beamadapter::RodStraightSection']]],
   ['rotateframeforalignnormalizedx_3',['RotateFrameForAlignNormalizedX',['../classbeamadapter_1_1_base_beam_interpolation.html#ae58c1d82e9c6a7831ae426db1145a797',1,'beamadapter::BaseBeamInterpolation']]],
-  ['rotateframeforalignx_4',['rotateframeforalignx',['../classbeamadapter_1_1_base_beam_interpolation.html#a899a756e92492f60fdf3694b9be0988a',1,'beamadapter::BaseBeamInterpolation::RotateFrameForAlignX()'],['../classbeamadapter_1_1_rod_mesh_section.html#af6e89e1da1340b2e44cfb491c09ac97f',1,'beamadapter::RodMeshSection::rotateFrameForAlignX()']]]
+  ['rotateframeforalignx_4',['rotateframeforalignx',['../classbeamadapter_1_1_rod_mesh_section.html#af6e89e1da1340b2e44cfb491c09ac97f',1,'beamadapter::RodMeshSection::rotateFrameForAlignX()'],['../classbeamadapter_1_1_base_beam_interpolation.html#a899a756e92492f60fdf3694b9be0988a',1,'beamadapter::BaseBeamInterpolation::RotateFrameForAlignX()']]]
 ];
